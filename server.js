c6d68a72186ad3bb17306078e84700f1370adf35
Tait Takatani <tait.takatani@gmail.com>
2017-11-19 16:32:07 -0800

    added env

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/scraper', {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
