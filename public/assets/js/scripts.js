6acb5e17c0cc04247d2f0bf7bb72a57097a23141
Tait Takatani <tait.takatani@gmail.com>
2017-11-20 10:12:26 -0800

    separated ajax calls for multiple uses

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,163 +1,155 @@
-//Get news
+//Click to scrape news
 $('#new').on('click', () => {
-  $.ajax({
-    method: 'GET',
-    url: '/scrape'
-  }).then( () => location.reload() );
+  scrapeArticle();
 });
 
-//Save article
+//Click to save article
 $('.save').on('click', function () {
-  const thisId = $(this).attr('data-id');
-  $.ajax({
-    method: 'POST',
-    url: `/save/${thisId}`,
-  }).then( () => location.reload() );
+  saveArticle($(this).attr('data-id'));
 });
 
-//Unsave article
+//Click to unsave article
 $('.unsave').on('click', function () {
-  const thisId = $(this).attr('data-id');
-  $.ajax({
-    method: 'POST',
-    url: `/unsave/${thisId}`,
-  }).then( () => location.reload() );
+  unsaveArticle($(this).attr('data-id'));
 });
 
-//Add note
+//Click to add note
 $('.plus').on('click', function () {
   const id = $(this).attr('id').slice(4);
   const $in = $(`#${id}`);
-
   if ($in.val().trim() === '') {
     $in.focus();
     $in.val('');
   } else {
-    $.ajax({
-      method: 'POST',
-      url: `/add/${id}`,
-      data: {
-        text: $in.val()
-      }
-    }).then( () => location.reload() );
-  }
+    addNote(id, $in.val());
+  }  
 });
 
-//Delete note
+//Click to delete note
 $('.minus').on('click', function () {
   const idArt = $(this).attr('id').slice(4);
-  const idNote = $(this).attr('data-id');
+  const idNote = $(`#sub-${idArt}`).attr('data-id');
   const $in = $(`#${idArt}`);
-
   if ($(this).hasClass('edit')) {
     if ($in.val().trim() === '') {
       $in.focus();
       $in.val('');
     } else {
-      $.ajax({
-        method: 'POST',
-        url: `/edit/${idNote}`,
-        data: {
-          text: $in.val()
-        }
-      }).then( () => location.reload() );
+      editNote(idNote, $in.val());
     }    
   } else {
-    $.ajax({
-      method: 'POST',
-      url: `/sub/${idArt}/${idNote}`
-    }).then( () => location.reload() );
+    delNote(idArt, idNote);
   } 
 });
 
-//Editing note
+//Click editing note
 $('.noted').on('click', function () {
-  const id = $(this).attr('id');
-  const idNote = $(`#sub-${id}`).attr('data-id');
-  const $in = $(`#${id}`);
+  const idArt = $(this).attr('id');
+  const idNote = $(`#sub-${idArt}`).attr('data-id');
+  addEdit(idArt, idNote);
+});
 
-  if ($(`#edit-${id}`).length === 0) {
+// Determine what to do if keys are pressed in input
+$('input').on('keyup', function (e) {
+  const idArt = $(this).attr('id');
+  const idNote = $(`#sub-${idArt}`).attr('data-id');
+  if (e.keyCode === 13) {
+    if ($(`#add-${idArt}`).length !== 0) {
+      if ($(this).val().trim() === '') {
+        $(this).focus();
+        $(this).val('');
+      } else {
+        addNote(idArt, $(this).val());
+      }      
+    } else if ($(`#edit-${idArt}`).length !== 0) {
+      if ($(this).val().trim() === ''
+          || $(this).val().trim() === $(this).attr('value')) {
+        $(this).val($(this).attr('value'));
+        $(this).blur();
+        $(`#edit-${idArt}`).remove();
+      } else {
+        editNote(idNote, $(this).val());
+      }
+    }
+  } else {
+    if ($(`#edit-${idArt}`).length === 0) {
+      addEdit(idArt, idNote);
+    }    
+  }
+});
+
+//Add Edit Button
+const addEdit = (idArt, idNote) => {
+  const $in = $(`#${idArt}`);
+  if ($(`#edit-${idArt}`).length === 0) {
     const $edit = $('<div>').addClass('edit').attr({
-      id: `edit-${id}`,
+      id: `edit-${idArt}`,
       'data-id': idNote
     }).html('✎').css('border', 'none');
 
-    $edit.insertAfter($(`#sub-${id}`)).on('click', function () {
+    $edit.insertAfter($(`#sub-${idArt}`)).on('click', function () {
       if ($in.val().trim() === ''
           || $in.val().trim() === $in.attr('value')) {
         $in.val($in.attr('value'));
         $(this).remove();
       } else {
-        $.ajax({
-          method: 'POST',
-          url: `/edit/${idNote}`,
-          data: {
-            text: $in.val()
-          }
-        }).then( () => location.reload() );
+        editNote(idNote, $in.val());
       }     
     })
   }
-});
+};
 
-// Determine what to do if Enter is pressed in input
-$('input').on('keyup', function (e) {
-  const id = $(this).attr('id');
-  if (e.keyCode === 13) {
-    if ($(`#add-${id}`).length !== 0) {
-      if ($(this).val().trim() === '') {
-        $(this).focus();
-        $(this).val('');
-      } else {
-        $.ajax({
-          method: 'POST',
-          url: `/add/${id}`,
-          data: {
-            text: $(this).val()
-          }
-        }).then( () => location.reload() );
-      }      
-    } else if ($(`#edit-${id}`).length !== 0) {
-      const idNote = $(`#sub-${id}`).attr('data-id');
-      if ($(this).val().trim() === ''
-          || $(this).val().trim() === $(this).attr('value')) {
-        $(this).val($(this).attr('value'));
-        $(this).blur();
-        $(`#edit-${id}`).remove();
-      } else {
-        $.ajax({
-          method: 'POST',
-          url: `/edit/${idNote}`,
-          data: {
-            text: $(this).val()
-          }
-        }).then( () => location.reload() );
-      }
+/***** Separate ajax calls for future multiple use *****/
+//Scrape Article
+const scrapeArticle = () => {
+  $.ajax({
+    method: 'GET',
+    url: '/scrape'
+  }).then( () => location.reload() );  
+};
+
+//Save Article
+const saveArticle = (id) => {
+  $.ajax({
+    method: 'POST',
+    url: `/save/${id}`,
+  }).then( () => location.reload() );
+};
+
+//Unsave Article
+const unsaveArticle = (id) => {
+  $.ajax({
+    method: 'POST',
+    url: `/unsave/${id}`,
+  }).then( () => location.reload() );
+};
+
+//Add Note
+const addNote = (id, text) => {
+  $.ajax({
+    method: 'POST',
+    url: `/add/${id}`,
+    data: {
+      text: text
     }
-  } else {
-    if ($(`#edit-${id}`).length === 0) {
-      const idNote = $(`#sub-${id}`).attr('data-id');
-      const $in = $(this);
-      const $edit = $('<div>').addClass('edit').attr({
-        id: `edit-${id}`,
-        'data-id': idNote
-      }).html('✎').css('border', 'none');
+  }).then( () => location.reload() );
+};
 
-      $edit.insertAfter($(`#sub-${id}`)).on('click', function () {
-        if ($in.val().trim() === ''
-            || $in.val().trim() === $in.attr('value')) {
-          $in.val($in.attr('value'));
-          $(this).remove();
-        } else {
-          $.ajax({
-            method: 'POST',
-            url: `/edit/${idNote}`,
-            data: {
-              text: $in.val()
-            }
-          }).then( () => location.reload() );
-        }     
-      })
-    }    
-  }
-});
+//Delete Note
+const delNote = (idArt, idNote) => {
+  $.ajax({
+    method: 'POST',
+    url: `/sub/${idArt}/${idNote}`
+  }).then( () => location.reload() );
+};
+
+//Edit Note
+const editNote = (idNote, text) => {
+  $.ajax({
+    method: 'POST',
+    url: `/edit/${idNote}`,
+    data: {
+      text: text
+    }
+  }).then( () => location.reload() );
+};
